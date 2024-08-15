219a8941c6d553c5dad73b74bd83fc1b7878f7f6
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-08-16 00:35:41 +0200

    sketched api

---
+++ Manager/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+cd "$SCRIPT_DIR" || exit 1
+
+python -m venv .venv
+source ./.venv/bin/activate
+pip install -r requirements.txt
---
+++ Manager/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+cd "$SCRIPT_DIR" || exit 1
+
+source ./.venv/bin/activate
+fastapi dev main.py
